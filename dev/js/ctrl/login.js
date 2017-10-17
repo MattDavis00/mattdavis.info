d288023fc88dff0b6ac7b8ccef3542fb2e06e5a7
Matt <mw.davis@hotmail.co.uk>
2017-10-17 18:06:11 +0100

    "login.js" AngularJS controller was added as a script and as a controller to "login.html".

--- dev/js/ctrl/login.js
+++ dev/js/ctrl/login.js
@@ -1,3 +1,16 @@
-app.controller('myCtrl', function($scope) {
+angular.module("my-app").controller("loginCtrl", ["$scope", "$http", function($scope, $http) {
 
-});
+  $scope.loginData = {};
+
+  $scope.url = "php/login.php";
+
+  $scope.processLogin = function() {
+    $http({
+     method: "post",
+     url: "php/login.php"
+    }).then(function successCallback(response) {
+     // Store response data
+     $scope.users = response.data;
+    });
+
+}]);
