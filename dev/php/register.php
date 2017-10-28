fa49b4f1a6a0b79d72d1d4dfa041ade51493c86b
Matt <mw.davis@hotmail.co.uk>
2017-10-28 21:16:17 +0100

    Salt field was added to the administrator table. Register form now hashes passwords using the BCRYPT algorithm taking password concatenated with a random salt as an input. The salt and generated hash are then stored in the database alongside the other information. Variables that originate from the user now begin with client. For example the variable that holds the email that the user input is now named $clientEmail.

--- dev/php/register.php
+++ dev/php/register.php
@@ -1,11 +1,12 @@
 <?php
 
-$postdata = file_get_contents("php://input");
-$request = json_decode($postdata);
-$userEmail = $request->email;
-$userPassword = $request->password;
-$userFirstName = $request->firstName;
-$userLastName = $request->lastName;
+$postData = file_get_contents("php://input");
+$request = json_decode($postData);
+
+$clientEmail = $request->email;
+$clientPassword = $request->password;
+$clientFirstName = $request->firstName;
+$clientLastName = $request->lastName;
 
 $servername = "localhost";
 $username = "root";
@@ -19,8 +20,11 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO administrator (Email, Org_ID, Hashed_Pass, First_Name, Last_Name, Last_Login, Creation_UNIX)
-VALUES ('$userEmail', 127836, '$userPassword', '$userFirstName', '$userLastName', 1786873927, 2783636)";
+$clientSalt = mcrypt_create_iv(16, MCRYPT_DEV_URANDOM);
+$clientPasswordHash = password_hash($clientPassword . $clientSalt, PASSWORD_BCRYPT);
+
+$sql = "INSERT INTO administrator (Email, Org_ID, Hashed_Pass, Salt, First_Name, Last_Name, Last_Login, Creation_UNIX)
+VALUES ('$clientEmail', 127836, '$clientPasswordHash', '$clientSalt', '$clientFirstName', '$clientLastName', 1786873927, 2783636)";
 
 if ($conn->query($sql) === TRUE) {
     echo "New record created successfully";
