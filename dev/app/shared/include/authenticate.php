893c5abd3b22eb63de93d920a82005e6d97192e2
Matt <mw.davis@hotmail.co.uk>
2017-11-15 14:02:14 +0000

    If the device is authenticated and the user is logged in then an "Athenticated" message is displayed, otherwise "No authentication." is displayed.

--- dev/app/shared/include/authenticate.php
+++ dev/app/shared/include/authenticate.php
@@ -1,9 +1,8 @@
 <?php
 
-if (($_SESSION["deviceAuth"] == true) && ($_SESSION["loggedIn"] == true)){
-
+if (($_SESSION["deviceAuth"] == true) && ($_SESSION["loggedIn"] == true)) {
+    echo " - Authenticated.";
 } else {
-
+    echo " - No authentication.";
+    die();
 }
-
-?>
