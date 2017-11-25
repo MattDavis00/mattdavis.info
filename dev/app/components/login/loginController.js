f889084eaceb3daf6aa9015771a0126ded4b0b22
Matt <mw.davis@hotmail.co.uk>
2017-11-25 16:17:45 +0000

    Authenticated elements are now only shown upon successful login. If the user is an administrator, instead of displaying a storeID, it will display administrator instead. Incorrect login: https://i.imgur.com/ayeEPqd.png Admin login: https://i.imgur.com/o1byte8.png

--- dev/app/components/login/loginController.js
+++ dev/app/components/login/loginController.js
@@ -32,8 +32,17 @@ angular.module("my-app").controller("loginCtrl", ["$scope", "$http", function($s
 
     request.then(function(response) {
       $scope.results = response.data;
+      var returnData = angular.fromJson(response.data);
+      if (returnData.verification) {
+        if (returnData.administrator) {
+          $("#storeID").html("Store ID<br>Administrator");
+        } else {
+          $("#storeID").html("Store ID<br>"+returnData.storeID);
+        }
+        $("#userID").html("Store ID<br>"+returnData.userID);
+        $(".authenticated-nav-elements").show();
+      }
       $scope.info = " - processLogin() function ran!";
-      $(".authenticated-nav-elements").show();
     });
   }
   $(".authenticated-nav-elements").hide();
