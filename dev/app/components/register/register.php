82b616e5fe7bfd8893d8a399ecd8b68769db33b3
Matt <mw.davis@hotmail.co.uk>
2017-11-30 16:54:15 +0000

    Formatting changes. If the registration was successful, then a boolean value is now outputted to the client.

--- dev/app/components/register/register.php
+++ dev/app/components/register/register.php
@@ -1,7 +1,7 @@
 <?php
 
 // Database Connection & Post data
-include ($_SERVER["DOCUMENT_ROOT"]."/dev/app/shared/include/connection.php");
+include($_SERVER["DOCUMENT_ROOT"]."/dev/app/shared/include/connection.php");
 
 // Input Variables
 $clientEmail = $request->email;
@@ -17,8 +17,18 @@ $clientPasswordHash = password_hash($clientPassword . $clientSalt, PASSWORD_BCRY
 $sql = "INSERT INTO administrator (Email, Org_ID, Hashed_Pass, Salt, First_Name, Last_Name, Last_Login_UNIX, Creation_UNIX)
 VALUES ('$clientEmail', NULL, '$clientPasswordHash', '$clientSalt', '$clientFirstName', '$clientLastName', NULL, $serverUNIX)";
 
-// Execute Query & Output
-databaseInsert($sql, $conn);
+// Execute Query
+$registerReturn = databaseInsert($sql, $conn);
+
+// Check Query
+if ($registerReturn["success"]) {
+    $outputArray["registerSuccess"] = true; // If the insert was successful, return true.
+} else {
+    $outputArray["registerSuccess"] = false; // If the insert failed, return false.
+}
+
+// Output
+echo json_encode($outputArray);
 
 // Close Connection
 $conn->close();
