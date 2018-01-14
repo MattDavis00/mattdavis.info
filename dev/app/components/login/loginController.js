3752bbac5a7f7c0f0299041da3d837af98ab8af1
Matt <mw.davis@hotmail.co.uk>
2018-01-14 21:34:57 +0000

    initialize.php was renamed to initialise.php

--- dev/app/components/login/loginController.js
+++ dev/app/components/login/loginController.js
@@ -5,7 +5,7 @@ angular.module("my-app").controller("loginCtrl", ["$scope", "$http", "localVaria
 
   var request = $http({
     method: "post",
-    url: "app/components/login/initialize.php",
+    url: "app/components/login/initialise.php",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
     }
@@ -13,7 +13,7 @@ angular.module("my-app").controller("loginCtrl", ["$scope", "$http", "localVaria
 
   request.then(function(response) {
     $scope.results = response.data;
-    $scope.info = " - initialize() function ran!";
+    $scope.info = " - initialise() function ran!";
   });
 
   $scope.processLogin = function() {
