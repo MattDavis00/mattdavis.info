4143e1a0ec0b03c237722c02f471cb61f208427f
Matt <mw.davis@hotmail.co.uk>
2018-01-29 14:08:41 +0000

    The `item` table in the database is now queried to display the results of the searchItem page, it nolonger uses a test JSON string of 4 objects. The database is queried, returning the actual data of that administrators organisation as a JSON encoded string, and then this is ng-repeated to display it to the user.

--- dev/app/components/inventory/search/searchItem.php
+++ dev/app/components/inventory/search/searchItem.php
@@ -11,65 +11,35 @@ $insertReturn = false;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Input Variables
-$clientStoreID = $request->storeID;
-$clientFirstName = $request->firstName;
-$clientLastName = $request->lastName;
+$data = array();
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Query database for any existing devices with this ID
-$storeSelect = $conn->prepare("SELECT `Org_ID` FROM `store` WHERE `Store_ID` = ? LIMIT 1");
-$storeSelect->bind_param("i", $clientStoreID);
+$storeSelect = $conn->prepare("SELECT `Item_ID`,`Name`,`Price`,`Description`,`Barcode` FROM `item` WHERE `Org_ID` = ?");
+$storeSelect->bind_param("i", $_SESSION["orgID"]);
 
 // Execute Query And Bind Results
 $storeSelect->execute();
 $storeSelect->store_result();
-$storeSelect->bind_result($serverOrg_ID);
-$storeSelect->fetch();
+$storeSelect->bind_result($serverItemID,$serverName,$serverPrice,$serverDescription,$serverBarcode);
+
+while ($storeSelect->fetch()) {
+  $tempData["itemID"] = $serverItemID;
+  $tempData["name"] = $serverName;
+  $tempData["price"] = $serverPrice;
+  $tempData["description"] = $serverDescription;
+  $tempData["barcode"] = $serverBarcode;
+
+  $data[] = $tempData;
+}
 
 // Close Statement
 $storeSelect->close();
 
-if ($serverOrg_ID == $_SESSION["orgID"]) { // If the Store_ID is found and the administrator is part of this organisation.
-
-    // Generate Random Password
-    $result = '';
-
-    for($i = 0; $i < 6; $i++) {
-        $result .= mt_rand(0, 9); // Generate Random 6 Digit Number
-    }
-
-    $clientPassword = (int)$result;
-
-    // Generate Salt & Hash
-    $clientSalt = bin2hex(random_bytes(8));
-    $clientPasswordHash = password_hash($result . $clientSalt, PASSWORD_BCRYPT);
-
-    // SQL Query
-    $stmt = $conn->prepare("INSERT INTO user (Store_ID, Hashed_Pass, Salt, First_Name, Last_Name, Last_Login_UNIX, Creation_UNIX)
-    VALUES (?, ?, ?, ?, ?, NULL, ?)");
-    $stmt->bind_param("issssi", $clientStoreID, $clientPasswordHash, $clientSalt, $clientFirstName, $clientLastName, $serverUNIX);
-
-    // Execute Query
-    $insertReturn = $stmt->execute();
-
-    // Store User ID
-    $serverUserID = $stmt->insert_id;
-
-    // Close Statement
-    $stmt->close();
-
-}
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Check Query
-if ($insertReturn) {
-    $outputArray["userID"] = $serverUserID; // Return the generated User ID.
-    $outputArray["password"] = $clientPassword; // Return the randomly generated 6 digit passcode.
-    $outputArray["insertSuccess"] = true; // If the insert was successful, return true.
-}
+$outputArray["selectData"] = $data; // Return the data.
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
--- dev/app/components/inventory/search/searchItemController.js
+++ dev/app/components/inventory/search/searchItemController.js
@@ -5,53 +5,17 @@ angular.module("my-app").controller("searchItemCtrl", ["$scope", "$http", "authC
   $scope.searchItemData = {};
   $scope.results = "";
 
-  $scope.sortType = 'id'; // set the default sort type
+  $scope.sortType = 'itemID'; // set the default sort type
   $scope.sortReverse = false; // set the default sort order
-
-  // create the list of sushi rolls
-  $scope.items = [{
-      id: 456231,
-      name: 'Nails',
-      price: 2.49,
-      description: "Oh this is a really nice and long description, I really hope this gets truncated some time soon. Oh this is a really nice and long description, I really hope this gets truncated some time soon. Oh this is a really nice and long description, I really hope this gets truncated some time soon.",
-      barcode: "45616168789165"
-    },
-    {
-      id: 626451,
-      name: 'Beans Beans Beans Beans Beans Beans Beans Beans',
-      price: 7.99,
-      description: "This is a different description!",
-      barcode: "245661419514591"
-    },
-    {
-      id: 784318,
-      name: 'Hammer',
-      price: 14.69,
-      description: "Wow I really would like a hammer for Christmas!",
-      barcode: "34915491549811"
-    },
-    {
-      id: 946832,
-      name: 'Pens',
-      price: 0.99,
-      description: "Damn, I'm out of pens again!",
-      barcode: "71491594519451"
-    }
-  ];
 
   $scope.ClearItem = function() {
     $scope.searchItemData.search = "";
   }
 
-  $scope.SearchItem = function(item) {
+  $scope.SearchItem = function() {
     var request = $http({
       method: "post",
       url: "app/components/inventory/search/searchItem.php",
-      data: {
-        storeID: $scope.searchItemData.storeID,
-        firstName: $scope.searchItemData.firstName,
-        lastName: $scope.searchItemData.lastName
-      },
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
       }
@@ -65,8 +29,11 @@ angular.module("my-app").controller("searchItemCtrl", ["$scope", "$http", "authC
   $scope.EditItem = function(item) {
 
     $scope.info = item.id;
+    $scope.SearchItem(); // After the item has been editted/updated, run the SearchItem() function to update the table of results.
 
   }
   $(".authenticated-nav-elements").hide(); // Ensure that normal user icons are hidden.
   $(".authenticated-nav-elements-administrator").show(); // Show the top-nav and side-nav administrator icons
+
+  $scope.SearchItem();
 }]);
