beaa7442415e2ee30fd394490a5df021443261c5
Matt <mw.davis@hotmail.co.uk>
2018-04-26 22:11:42 +0100

    Client-side validation has been added to the register form in preparation for AJAX request functionality. New validation functions and error tooltip functions have been added. The client will send requests with the data alongside which element/input ID the data came from, this will allow PHP to return input specific errors to a specified input field.

--- project/app.js
+++ project/app.js
@@ -2,8 +2,8 @@ var app = angular.module("project-app", ["ngRoute"]);
 
 app.config(function($routeProvider) {
   $routeProvider
-    ///////////////////////// General ////////////////////////////
 
+    ///////////////////////// General ////////////////////////////
     .when("/", {
       templateUrl: "project/components/landing/landingView.html",
       controller: "landingCtrl"
@@ -13,4 +13,151 @@ app.config(function($routeProvider) {
     .otherwise({
       redirectTo: "/"
     })
+
+});
+
+app.service('sharedFunctions', function() {
+
+  this.Validation = {};
+
+  this.Validation.Email = function(element, email) {
+    var returnData = {};
+    returnData.errorFlag = false;
+    returnData.errors = "";
+
+    self.Validation.RemoveErrorTooltip(element);
+
+    if (!(email)) {
+      returnData.errorFlag = true;
+      returnData.errors += "Please enter your email. ";
+    } else {
+      if (!(email.length > 0)) {
+        returnData.errorFlag = true;
+        returnData.errors += "Please enter your email. ";
+      }
+
+      if (email.length > 100) {
+        returnData.errorFlag = true;
+        returnData.errors += "Email exceeds 100 characters. ";
+      }
+
+      if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/.test(email))) {
+        returnData.errorFlag = true;
+        returnData.errors += "Email is not valid. ";
+      }
+    }
+
+    if (returnData.errorFlag) {
+      self.Validation.ErrorTooltip(element, returnData.errors);
+    }
+
+    return returnData.errorFlag;
+
+  }
+
+  this.Validation.Name = function(element, name) {
+    var returnData = {};
+    returnData.errorFlag = false;
+    returnData.errors = "";
+
+    self.Validation.RemoveErrorTooltip(element);
+
+    if (!(name)) {
+      returnData.errorFlag = true;
+      returnData.errors += "Please enter your name. ";
+    } else {
+      if (!(name.length > 0)) {
+        returnData.errorFlag = true;
+        returnData.errors += "Please enter your name. ";
+      }
+
+      if (name.length > 50) {
+        returnData.errorFlag = true;
+        returnData.errors += "Name exceeds 50 characters. ";
+      }
+    }
+
+    if (returnData.errorFlag) {
+      self.Validation.ErrorTooltip(element, returnData.errors);
+    }
+
+    return returnData.errorFlag;
+
+  }
+
+  this.Validation.Password = function(element, element2, password, passwordRepeat) {
+    var returnData = {};
+    returnData.errorFlag = false;
+    returnData.errors = "";
+
+    self.Validation.RemoveErrorTooltip(element);
+    self.Validation.RemoveErrorTooltip(element2);
+
+    if (!(password)) {
+      returnData.errorFlag = true;
+      returnData.errors += "Please enter a password. ";
+    } else {
+      if (!(password.length >= 8)) {
+        returnData.errorFlag = true;
+        returnData.errors += "Your password must be atleast 8 characters long. ";
+      }
+
+      if (password !== passwordRepeat) {
+        returnData.errorFlag = true;
+        returnData.errors += "Passwords do not match. ";
+      }
+    }
+
+    if (returnData.errorFlag) {
+      self.Validation.ErrorTooltip(element, returnData.errors);
+      $(element2).addClass("error-border");
+    }
+
+    return returnData.errorFlag;
+
+  }
+
+  this.Validation.Empty = function(element, value) {
+    var returnData = {};
+    returnData.errorFlag = false;
+    returnData.errors = "";
+
+    self.Validation.RemoveErrorTooltip(element);
+
+    if (!(value)) {
+      returnData.errorFlag = true;
+      returnData.errors += "Please fill in this field. ";
+    } else {
+      if (!(value.length > 0)) {
+        returnData.errorFlag = true;
+        returnData.errors += "Please fill in this field. ";
+      }
+    }
+
+    if (returnData.errorFlag) {
+      self.Validation.ErrorTooltip(element, returnData.errors);
+    }
+
+    return returnData.errorFlag;
+
+  }
+
+  this.Validation.ErrorTooltip = function(element, errors) {
+    $(element).removeClass("error-border");
+    $(element).tooltip('dispose');
+    $(element).tooltip({
+      placement: 'top',
+      title: errors
+    });
+    $(element).tooltip('show');
+    $(element).addClass("error-border");
+  }
+
+  this.Validation.RemoveErrorTooltip = function(element) {
+    $(element).removeClass("error-border");
+    $(element).tooltip('dispose');
+  }
+
+  var self = this;
+
 });
--- project/components/landing/landingController.js
+++ project/components/landing/landingController.js
@@ -1,46 +1,74 @@
-angular.module("project-app").controller("landingCtrl", ["$scope", "$http", function($scope, $http) {
+angular.module("project-app").controller("landingCtrl", ["$scope", "$http", "sharedFunctions", "$filter", function($scope, $http, sharedFunctions, $filter) {
 
-  // $scope.loginData = {};
-  // $scope.results = "";
-  //
-  // $scope.processLogin = function() {
-  //
-  //   var request = $http({
-  //     method: "post",
-  //     url: "app/components/login/login.php",
-  //     data: {
-  //       id: $scope.loginData.id,
-  //       password: $scope.loginData.password
-  //     },
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded"
-  //     }
-  //   });
-  //
-  //   request.then(function(response) {
-  //     var returnData = angular.fromJson(response.data);
-  //
-  //     sessionStorage.userID = returnData.userID;
-  //     sessionStorage.storeID = returnData.storeID;
-  //     sessionStorage.orgID = returnData.orgID;
-  //     sessionStorage.administrator = returnData.administrator;
-  //     sessionStorage.loggedIn = returnData.loggedIn;
-  //
-  //     if (returnData.verification) {
-  //       if (returnData.administrator) {
-  //         sessionStorage.storeID = "Administrator";
-  //         if (returnData.orgID === null) {
-  //           window.location.href = '#!organisation'; // If the administrator is not part of an organisation, take them to the organisation view.
-  //         } else {
-  //           window.location.href = '#!credentials-search'; // Upon successful admin login, user is redirected to the query page.
-  //         }
-  //       } else {
-  //         window.location.href = '#!query-search'; // Upon successful user login, user is redirected to the query page.
-  //       }
-  //     }
-  //     $scope.info = " - processLogin() function ran!";
-  //   });
-  // }
+  $scope.registerData = {};
+  $scope.results = "";
+
+  $scope.RegisterUser = function() {
+
+    //////////////Validation Checks//////////////
+
+    var errorWithInput = false;
+
+    if (sharedFunctions.Validation.Email("#register-email", $scope.registerData.email)) {
+      errorWithInput = true;
+    }
+    if (sharedFunctions.Validation.Name("#register-firstName", $scope.registerData.firstName)) {
+      errorWithInput = true;
+    }
+    if (sharedFunctions.Validation.Name("#register-lastName", $scope.registerData.lastName)) {
+      errorWithInput = true;
+    }
+    if (sharedFunctions.Validation.Password("#register-password", "#register-passwordRepeat", $scope.registerData.password, $scope.registerData.passwordRepeat)) {
+      errorWithInput = true;
+    }
+
+    //////////////Register Request//////////////
+
+    if (!errorWithInput) {
+
+      var request = $http({
+        method: "post",
+        url: "project/components/landing/register.php",
+        data: {
+          email: [$scope.registerData.email, "#register-email"],
+          firstName: [$scope.registerData.firstName, "#register-firstName"],
+          lastName: [$scope.registerData.lastName, "#register-lastName"],
+          password: [$scope.registerData.password, "#register-password"],
+          passwordRepeat: [$scope.registerData.passwordRepeat, "#register-passwordRepeat"]
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+
+      request.then(function(response) {
+        var serverResponse = angular.fromJson(response.data);
+
+        // sessionStorage.userID = returnData.userID;
+        // sessionStorage.storeID = returnData.storeID;
+        // sessionStorage.orgID = returnData.orgID;
+        // sessionStorage.administrator = returnData.administrator;
+        // sessionStorage.loggedIn = returnData.loggedIn;
+
+        // if (returnData.verification) {
+        //   if (returnData.administrator) {
+        //     sessionStorage.storeID = "Administrator";
+        //     if (returnData.orgID === null) {
+        //       window.location.href = '#!organisation'; // If the administrator is not part of an organisation, take them to the organisation view.
+        //     } else {
+        //       window.location.href = '#!credentials-search'; // Upon successful admin login, user is redirected to the query page.
+        //     }
+        //   } else {
+        //     window.location.href = '#!query-search'; // Upon successful user login, user is redirected to the query page.
+        //   }
+        // }
+        // $scope.info = " - processLogin() function ran!";
+
+      });
+
+    }
+
+  }
 
   particlesJS.load('background-animation', 'particles.json');
 
