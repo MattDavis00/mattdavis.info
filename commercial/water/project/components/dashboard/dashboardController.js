63b8641f86996665fb522158c714da12d373e6b8
Matt <mw.davis@hotmail.co.uk>
2018-06-29 12:52:55 +0100

    Dashboard was updated to use javascript data, repeating html attributes for all of the available sites.

--- commercial/water/project/components/dashboard/dashboardController.js
+++ commercial/water/project/components/dashboard/dashboardController.js
@@ -1,34 +1,74 @@
 angular.module("project-app").controller("dashboardCtrl", ["$scope", "$http", "sharedFunctions", "$filter", "localVariables", function($scope, $http, sharedFunctions, $filter, localVariables) {
 
-  $scope.Modal = {};
+  $scope.siteDashboard = {};
   $scope.results = "";
 
-  $scope.Modal.SwitchLoginRegister = function() {
-    $('#loginModal').modal('toggle');
-    $('#registerModal').modal('toggle');
-  }
-
-  $('#loginModal').on('hidden.bs.modal', function(e) {
-    $scope.loginData = {};
-    sharedFunctions.Validation.RemoveErrorTooltip('#login-email');
-    sharedFunctions.Validation.RemoveErrorTooltip('#login-password');
-  })
-
-  $('#registerModal').on('hidden.bs.modal', function(e) {
-    $scope.registerData = {};
-    sharedFunctions.Validation.RemoveErrorTooltip('#register-email');
-    sharedFunctions.Validation.RemoveErrorTooltip('#register-firstName');
-    sharedFunctions.Validation.RemoveErrorTooltip('#register-lastName');
-    sharedFunctions.Validation.RemoveErrorTooltip('#register-password');
-    sharedFunctions.Validation.RemoveErrorTooltip('#register-passwordRepeat');
-  })
-
-  $scope.ClearForms = function() {
-    $scope.loginData = {};
-    $scope.registerData = {};
-    sharedFunctions.Validation.RemoveErrorTooltip();
-  }
-
   $scope.sharedFunctions = sharedFunctions;
+
+  $scope.siteDashboard.sites = [{
+      name: "The Shard",
+      address: "32 London Bridge St, London",
+      description: "Some descriptive information!",
+      tag: "alert"
+    },
+    {
+      name: "Heron Tower",
+      address: "110 Bishopsgate, London",
+      description: "Some descriptive information!",
+      tag: "new"
+    },
+    {
+      name: "Aeron Tower",
+      address: "110 Bishopsgate, London",
+      description: "Some descriptive information!",
+      tag: "new"
+    },
+    {
+      name: "Broadgate Tower",
+      address: "201 Bishopsgate, London",
+      description: "Some descriptive information!",
+      tag: "normal"
+    },
+    {
+      name: "Troadgate Tower",
+      address: "201 Bishopsgate, London. 201 Bishopsgate, London. 201 Bishopsgate, London. 201 Bishopsgate, London.",
+      description: "Some descriptive information!",
+      tag: "normal"
+    },
+    {
+      name: "And Shard",
+      address: "32 London Bridge St, London",
+      description: "Some descriptive information!",
+      tag: "alert"
+    },
+    {
+      name: "Proadgate Tower",
+      address: "201 Bishopsgate, London",
+      description: "Some descriptive information!",
+      tag: "normal"
+    },
+    {
+      name: "Droadgate Tower",
+      address: "201 Bishopsgate, London",
+      description: "Some descriptive information!",
+      tag: "normal"
+    },
+    {
+      name: "Froadgate Tower",
+      address: "201 Bishopsgate, London",
+      description: "Some descriptive information!",
+      tag: "normal"
+    }
+  ];
+
+  $scope.siteDashboard.displayedSites = $scope.siteDashboard.sites;
+
+  $scope.$watch('[siteDashboard.searchData]', function() {
+    if ($scope.siteDashboard.sites != null) { // This function is called on load and every time a variable is changed, to prevent this code causing an error whilst waiting for the post request, this stops the code running on load.
+      $scope.temp = $filter('filter')($scope.siteDashboard.sites, $scope.siteDashboard.searchData); // Filter the search query in the search bar.
+      $scope.temp = $filter('orderBy')($scope.temp, ['tag', 'name']); // Filter by column.
+      $scope.siteDashboard.displayedSites = $scope.temp;
+    }
+  });
 
 }]);
