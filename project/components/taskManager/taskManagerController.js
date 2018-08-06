1d0c91dc38c6dbb24c2e520f517df375cd1cc435
Matt <mw.davis@hotmail.co.uk>
2018-08-06 18:37:03 +0100

    Tags have been added to task manager cards, a new tag preferences modal has been added which will allow the user to change each tag's text.

--- project/components/taskManager/taskManagerController.js
+++ project/components/taskManager/taskManagerController.js
@@ -1,24 +1,61 @@
 angular.module("project-app").controller("taskManagerCtrl", ["$scope", "$http", "sharedFunctions", "$filter", "localVariables", function($scope, $http, sharedFunctions, $filter, localVariables) {
 
   $scope.Modal = {};
-  $scope.pastebinData = {};
+  $scope.taskData = {};
   $scope.results = "";
 
-  // var el = document.getElementById('items');
-  // var sortable = new Sortable(el, {
-  //   animation: 150 // ms, animation speed moving items when sorting, `0` — without animation
-  // });
+  $scope.pillData = [{
+      class: "badge-primary",
+      text: "New"
+    },
+    {
+      class: "badge-secondary",
+      text: "Uni"
+    },
+    {
+      class: "badge-success",
+      text: "Work"
+    },
+    {
+      class: "badge-danger",
+      text: "ASAP"
+    },
+    {
+      class: "badge-warning",
+      text: "Important"
+    },
+    {
+      class: "badge-info",
+      text: "Taxes"
+    },
+    {
+      class: "badge-light",
+      text: "Today"
+    },
+    {
+      class: "badge-dark",
+      text: "Comp Sci"
+    }
+  ];
 
-  $(function() {
-    $("#items").sortable({
-      revert: 200,
-      opacity: 0.5,
-      containment: "parent",
-      tolerance: "pointer",
-      handle: ".item-handle"
-    });
-    $("#items").disableSelection();
-  });
+  $scope.tempPillData = angular.copy($scope.pillData);
+
+  $scope.taskArray = [{
+      title: "Further Maths Sucks",
+      description: "This is a really heart felt description about how Further Maths is really bad.",
+      tags: [0, 2, 4]
+    },
+    {
+      title: "Computer Science Is Alright",
+      description: "Another description right here!",
+      tags: [1, 5]
+    },
+    {
+      title: "Maths Was Great",
+      description: "Townshend saved our souls, thank you.",
+      tags: [3, 6, 7]
+    }
+  ];
 
   $scope.sharedFunctions = sharedFunctions;
 
