d145da22a341180aad2a6cf7c2fe667fa13b2d30
Matt <mw.davis@hotmail.co.uk>
2018-08-09 17:07:13 +0100

    The newTask.php script is complete with validation, insertion and generating and returning an error/success report.

--- project/components/taskManager/newTask.php
+++ project/components/taskManager/newTask.php
@@ -12,90 +12,66 @@ include($_SERVER["DOCUMENT_ROOT"]."/project/shared/authenticate.php");
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Input Variables
-$clientCode = $request->code;
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Success Flags
-$outputData->loginSuccess = false;
+$clientTitle = $request->title;
+$clientDescription = $request->description;
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ///// Validations /////
 
-// Code
-$codeValidation = $validate->Empty($clientCode->data); // Run validation
-if ($codeValidation->errorFlag) // If there was en error with the email.
+// Title
+$titleValidation = $validate->LengthAndEmpty($clientTitle->data, 50); // Run validation
+if ($titleValidation->errorFlag) // If there was en error with the email.
 {
   $outputData->errorFlag = true;
 
   $errorEntry = new StdClass();
-  $errorEntry->field = $clientCode->field;
-  $errorEntry->errorMessage = $codeValidation->errorMessage;
+  $errorEntry->field = $clientTitle->field;
+  $errorEntry->errorMessage = $emailValidation->errorMessage;
 
   $outputData->errorReport[] = $errorEntry;
 }
+// Description
+$descriptionValidation = $validate->Length($clientDescription->data, 1000); // Run validation
+if ($descriptionValidation->errorFlag) // If there was en error with the email.
+{
+  $outputData->errorFlag = true;
+
+  $errorEntry = new StdClass();
+  $errorEntry->field = $clientDescription->field;
+  $errorEntry->errorMessage = $descriptionValidation->errorMessage;
+
+  $outputData->errorReport[] = $errorEntry;
+}
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 if (!$outputData->errorFlag)
 {
 
-  for ($i = 0; $i < 5; $i++)
+  try
   {
 
-    // Generate Random Char ID
-    $charID = $utility->RandomString(6);
-
     // SQL Query
-    $selectPaste = $conn->prepare("SELECT `Paste_ID` FROM `paste` WHERE `Char_ID` = :charID");
-    $selectPaste->bindParam(':charID', $charID);
+    $createTask = $conn->prepare("INSERT INTO `task` (`User_ID`, `Title`, `Description`, `Creation_Time`)
+    VALUES (:userID, :title, :description, :creationTime)");
+    $createTask->bindParam(':userID', $_SESSION["userID"]);
+    $createTask->bindParam(':title', $clientTitle->data);
+    $createTask->bindParam(':description', $clientDescription->data);
+    $createTask->bindParam(':creationTime', $serverDateTime);
 
     // Execute Query
-    $selectPasteReturn = $selectPaste->execute();
-
-    $result = $selectPaste->fetchAll(\PDO::FETCH_ASSOC);
-
-    // Close Statement
-    $selectPaste = null;
-
-    if (count($result) === 0)
-    {
-      try
-      {
-
-        // SQL Query
-        $insertPaste = $conn->prepare("INSERT INTO `paste` (`Char_ID`, `Code`, `User_ID`, `Creation_Time`)
-        VALUES (:charID, :code, :userID, :creationTime)");
-        $insertPaste->bindParam(':charID', $charID);
-        $insertPaste->bindParam(':code', $clientCode->data);
-        $insertPaste->bindParam(':userID', $_SESSION["userID"]);
-        $insertPaste->bindParam(':creationTime', $serverDateTime);
-
-        // Execute Query
-        $insertPasteReturn = $insertPaste->execute();
-
-        $outputData->shareSuccess = true;
-        $outputData->shareCharID = $charID;
-        $i = 5;
-
-      }
-      catch(PDOException $e)
-      {
-        $outputData->executionErrorFlag = true;
-        $outputData->executionError = "Share failed. Please try again. ";
-      }
-    }
-    else if ($i === 4)
-    {
-      $outputData->executionErrorFlag = true;
-      $outputData->executionError = "Cannot generate ID for paste. ";
-    }
-
+    $createTaskReturn = $createTask->execute();
+  }
+  catch(PDOException $e)
+  {
+    $outputData->executionErrorFlag = true;
+    $outputData->executionError = "Could not create new task. Please try again. ";
   }
 
   // Close Statement
-  $insertPaste = null;
+  $createTask = null;
 
 }
 
