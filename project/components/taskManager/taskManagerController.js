90e41cf52146b4c03041061c84eccada5185c410
Matt <mw.davis@hotmail.co.uk>
2018-08-09 17:06:27 +0100

    The client-side code for creating a new task has been completed. This includes using the new validation methods alongside a post request to the server. After this an error report is processed, if no errors were returned then a success prompt is displayed.

--- project/components/taskManager/taskManagerController.js
+++ project/components/taskManager/taskManagerController.js
@@ -57,6 +57,70 @@ angular.module("project-app").controller("taskManagerCtrl", ["$scope", "$http", 
     }
   ];
 
+  $scope.NewTask = function() {
+
+    var errorWithInput = false;
+
+    // Check the title field is not empty and does not exceed 50 characters.
+    if (sharedFunctions.Validation.LengthAndEmpty("#task-title", $scope.taskData.title, 50)) {
+      errorWithInput = true;
+    }
+
+    // Check that the description field does not exceed the maximum amount of characters, this is allowed to be left empty for convenience.
+    if (sharedFunctions.Validation.Length("#task-description", $scope.taskData.description, 1000)) {
+      errorWithInput = true;
+    }
+
+
+    ////////////// New Task Request //////////////
+
+    if (!errorWithInput) {
+
+      var request = $http({
+        method: "post",
+        url: "project/components/taskManager/newTask.php",
+        data: {
+          title: {
+            data: $scope.taskData.title,
+            field: "#task-title"
+          },
+          description: {
+            data: $scope.taskData.description,
+            field: "#task-description"
+          }
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      });
+
+      // Take return error/success information. Based off of this either produce an error or success prompt.
+
+      request.then(function(response) {
+        var serverResponse = angular.fromJson(response.data);
+
+        if (serverResponse.errorFlag) // There were errors found
+        {
+          var fields = ["#task-title", "#task-description"];
+          for (var i = 0; i < fields.length; i++) {
+            sharedFunctions.Validation.RemoveErrorTooltip(fields[i]);
+          }
+
+          for (var i = 0; i < serverResponse.errorReport.length; i++) {
+            sharedFunctions.Validation.ErrorTooltip(serverResponse.errorReport[i].field, serverResponse.errorReport[i].errorMessage);
+          }
+        } else if (serverResponse.executionErrorFlag) { // Server could not insert
+          sharedFunctions.Prompt("error", serverResponse.executionError);
+        } else {
+          sharedFunctions.Prompt("success", "New task created!");
+        }
+
+      });
+
+    }
+
+  }
+
   $scope.AddPill = function(task, index) {
     if ($.inArray(index, task.tags) === -1) {
       task.tags.push(index);
