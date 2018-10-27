550bd3353875412714886cde05af8f670bd1787c
Matt <mw.davis@hotmail.co.uk>
2018-10-27 23:16:36 +0100

    The settings page now appropriately displays error messages received from server-side validation. The error text is also removed if the user then inputs valid information. If the password change is successful then the settings page details are cleared.

--- project/components/dashboard/dashboardController.js
+++ project/components/dashboard/dashboardController.js
@@ -29,10 +29,26 @@ angular.module("project-app").controller("dashboardCtrl", ["$scope", "$http", "s
     request.then(function(response) {
       var serverResponse = angular.fromJson(response.data);
 
+      var fields = ["#settings-password", "#settings-password-repeat"];
+      for (var i = 0; i < fields.length; i++) {
+        sharedFunctions.Validation.RemoveErrorTooltip(fields[i]);
+      }
+
       if (serverResponse.executionErrorFlag) { // Server could not insert
         sharedFunctions.Prompt("error", serverResponse.executionError);
       } else if (serverResponse.updateSuccess) {
         sharedFunctions.Prompt("success", "Settings updated successfully.");
+
+        $scope.settingsData.password = $scope.settingsData.passwordRepeat = "";
+      } else if (serverResponse.errorFlag) {
+
+        for (var i = 0; i < serverResponse.errorReport.length; i++) {
+          sharedFunctions.Validation.ErrorTooltip(serverResponse.errorReport[i].field, serverResponse.errorReport[i].errorMessage);
+
+          if (serverResponse.errorReport[i].field === "#settings-password") {
+            $("#settings-password-repeat").addClass("error-border");
+          }
+        }
       } else {
         sharedFunctions.Prompt("warning", "Unexpected response.");
       }
